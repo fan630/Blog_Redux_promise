715b8060b2b21ef6d374da572698ef8c16b8c738
fan630 <48875309+fan630@users.noreply.github.com>
2019-12-05 00:24:41 +0800

    Updates

--- precache-manifest.e81e138221bc65bd0485effa90cf1579.js
+++ precache-manifest.e81e138221bc65bd0485effa90cf1579.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d36706ff1b785317da8ed35d86eedd81",
+    "revision": "b0b0058407f14541841a104f7ca2f51c",
     "url": "/Blog_Redux_promise/index.html"
   },
   {
-    "revision": "1c13181d31f4c3178ba3",
-    "url": "/Blog_Redux_promise/static/css/main.d5675524.chunk.css"
+    "revision": "1444805b964ecb65ea38",
+    "url": "/Blog_Redux_promise/static/css/main.a334dad0.chunk.css"
   },
   {
     "revision": "6ffd8f6af78ea1fdc973",
     "url": "/Blog_Redux_promise/static/js/2.27418da5.chunk.js"
   },
   {
-    "revision": "1c13181d31f4c3178ba3",
+    "revision": "1444805b964ecb65ea38",
     "url": "/Blog_Redux_promise/static/js/main.f8526f8c.chunk.js"
   },
   {
